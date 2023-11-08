5be0f649aa5692daf9d70923bc619f655b970470
Oleg <773o@mail.ru>
2023-11-08 17:48:57 +0700

    Initial Commit

---
+++ TCANavigation/TCANavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCANavigationApp.swift
+//  TCANavigation
+//
+//  Created by Oleg Kolbasa on 08.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct TCANavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
